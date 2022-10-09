17267ccfce166005accd8e83e92281c3999a431f
Golban-Julia <golden95juli@gmail.com>
2022-10-09 16:57:57 +0300

    Added solution

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import Title from './components/Title';
 
 function App() {
+
+  const [isOpened, setOpened] = useState(false);
+  
+  const handleClick = () => {
+    setOpened(true);
+  };
+
+  if (!isOpened) {
+    return <button onClick={handleClick}>Open title</button>;
+  }
+   
   return (
     <div>
       <Title  name="Products" type="bold" />
-      <Title  name="Employees" type="normal" />
+      <Title name="Employees" type="normal" />
     </div>
   );
 }
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,9 +1,17 @@
+import PropTypes from "prop-types";
+
 const Title = ({ name, type }) => {
     return (
         <h1 style={{ fontWeight: type === "bold" ? "bold" : "normal" }}>
             {name}
         </h1>
    );
- 
 };
+
+
+Title.propTypes = {
+    name: PropTypes.string.isRequired,
+    type: PropTypes.oneOf(["bold", "normal"]),
+};
+
 export default Title;
